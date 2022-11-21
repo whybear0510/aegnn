20a8e876319ab39aef2031f25268c75acc4ff42a
whybear0510 <827239805@qq.com>
2022-11-21 22:55:24 +0100

    change back to wandb; add support for init_lr arg

--- scripts/train.py
+++ scripts/train.py
@@ -3,9 +3,11 @@ import datetime
 import os
 import pytorch_lightning as pl
 import pytorch_lightning.loggers
-# import wandb
+import wandb
 
 import aegnn
+import torch_geometric
+import torch
 
 
 def parse_args():
@@ -22,11 +24,17 @@ def parse_args():
     group.add_argument("--gradient_clip_val", default=0.0, type=float)
     group.add_argument("--limit_train_batches", default=1.0, type=int)
     group.add_argument("--limit_val_batches", default=1.0, type=int)
-
+    
+    parser.add_argument("--init_lr", default=0.001, type=float)
     parser.add_argument("--log-gradients", action="store_true")
     parser.add_argument("--profile", action="store_true")
     parser.add_argument("--debug", action="store_true") # == default is false
     parser.add_argument("--gpu", default=None, type=int)
+
+    #copy from flops.py
+    parser.add_argument("--radius", default=3.0, help="radius of radius graph generation")
+    parser.add_argument("--max-num-neighbors", default=32, help="max. number of neighbors in graph")
+    parser.add_argument("--pooling-size", default=(10, 10))
 
     parser = aegnn.datasets.EventDataModule.add_argparse_args(parser)
     return parser.parse_args()
@@ -34,29 +42,32 @@ def parse_args():
 
 def main(args):
 
-    # check data using wandb
-    # wandb.init(project="aegnn", entity="yyfteam")
-    # log_settings = wandb.Settings(start_method="thread")
-
     log_dir = os.environ["AEGNN_LOG_DIR"]
     loggers = [aegnn.utils.loggers.LoggingLogger(None if args.debug else log_dir, name="debug")]
     project = f"aegnn-{args.dataset}-{args.task}"
     experiment_name = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
+    # check data using wandb
+    runs_name = f'lr{args.init_lr}_batch{args.batch_size}_{experiment_name}'
+    wandb.init(project="aegnn", entity="yyfteam", name=runs_name)
+    # log_settings = wandb.Settings(start_method="thread")  # for Windows?
+
     dm = aegnn.datasets.by_name(args.dataset).from_argparse_args(args)
     dm.setup()
     model = aegnn.models.by_task(args.task)(args.model, args.dataset, num_classes=dm.num_classes,
-                                            img_shape=dm.dims, dim=args.dim, bias=True, root_weight=True)
+                                            img_shape=dm.dims, dim=args.dim, learning_rate=args.init_lr, bias=True, root_weight=True)
+    
+    # training the async model (?), copy from flop.py
+    # edge_attr = torch_geometric.transforms.Cartesian(cat=False, max_value=10.0)
+    # model = aegnn.asyncronous.make_model_asynchronous(model, args.radius, list(dm.dims), edge_attr)
 
     if not args.debug:
-        # wandb_logger = pl.loggers.WandbLogger(project=project, save_dir=log_dir, settings=log_settings)
-        # if args.log_gradients:
-        #     wandb_logger.watch(model, log="gradients")  # gradients plot every 100 training batches
-        # loggers.append(wandb_logger)
-        tb_logger = pl.loggers.TensorBoardLogger(log_dir, name=project)
-        # if args.log_gradients:
-        #     tb_logger.watch(model, log="gradients")  # gradients plot every 100 training batches
-        loggers.append(tb_logger)
+        wandb_logger = pl.loggers.WandbLogger(project=project, save_dir=log_dir)
+        if args.log_gradients:
+            wandb_logger.watch(model, log="gradients")  # gradients plot every 100 training batches
+        loggers.append(wandb_logger)
+
+
     logger = pl.loggers.LoggerCollection(loggers)
 
     checkpoint_path = os.path.join(log_dir, "checkpoints", args.dataset, args.task, experiment_name)
