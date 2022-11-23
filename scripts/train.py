2cf9c2ae0d354598ae852b679c4542aa75d44919
whybear0510 <827239805@qq.com>
2022-11-23 06:42:50 +0100

    support wandb sweep

--- scripts/train.py
+++ scripts/train.py
@@ -4,6 +4,7 @@ import os
 import pytorch_lightning as pl
 import pytorch_lightning.loggers
 import wandb
+import yaml
 
 import aegnn
 import torch_geometric
@@ -27,11 +28,13 @@ def parse_args():
     
     parser.add_argument("--log-gradients", action="store_true")
     parser.add_argument("--profile", action="store_true")
-    parser.add_argument("--debug", action="store_true") # == default is false
-    parser.add_argument("--gpu", default=None, type=int)
+    parser.add_argument("--debug", action="store_true") 
+    parser.add_argument("--cpu", action="store_true") # default is NOT using cpu
+    # parser.add_argument("--gpu", default=None, type=int)
 
     parser.add_argument("--init-lr", default=0.001, type=float)
     parser.add_argument("--weight-decay", default=0.005, type=float)
+    parser.add_argument("--wandb-sweep", action="store_true")
 
     #copy from flops.py
     parser.add_argument("--radius", default=3.0, help="radius of radius graph generation")
@@ -48,16 +51,51 @@ def main(args):
     loggers = [aegnn.utils.loggers.LoggingLogger(None if args.debug else log_dir, name="debug")]
     project = f"aegnn-{args.dataset}-{args.task}"
     experiment_name = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # check data using wandb
     runs_name = f'lr{args.init_lr}_batch{args.batch_size}_{experiment_name}'
-    wandb.init(project="aegnn", entity="yyfteam", name=runs_name)
-    # log_settings = wandb.Settings(start_method="thread")  # for Windows?
 
     dm = aegnn.datasets.by_name(args.dataset).from_argparse_args(args)
     dm.setup()
-    model = aegnn.models.by_task(args.task)(args.model, args.dataset, num_classes=dm.num_classes,
-                                            img_shape=dm.dims, dim=args.dim, learning_rate=args.init_lr, weight_decay = args.weight_decay, bias=True, root_weight=True)
+
+
+
+    if args.wandb_sweep:
+        with open('./wandb_sweep.yaml') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        run = wandb.init(config=config, project="aegnn", entity="yyfteam", name=runs_name)
+        # log_settings = wandb.Settings(start_method="thread")  # for Windows?
+
+        train_args = {
+            'max_epochs': wandb.config.max_epochs,
+            'learning_rate': wandb.config.learning_rate,
+            'weight_decay': wandb.config.weight_decay,
+            'batch_size': wandb.config.batch_size
+        }
+    else:
+        wandb.init(project="aegnn", entity="yyfteam", name=runs_name)
+        # log_settings = wandb.Settings(start_method="thread")  # for Windows?
+
+        train_args = {
+            'max_epochs': args.max_epochs,
+            'learning_rate': args.init_lr,
+            'weight_decay': args.weight_decay,
+            'batch_size': args.batch_size
+        }
+        
+    model_args = {
+        'network': args.model,
+        'dataset': args.dataset,
+        'num_classes': dm.num_classes,
+        'img_shape': dm.dims,
+        'dim': args.dim,
+        'learning_rate': train_args["learning_rate"], 
+        'weight_decay': train_args["weight_decay"], 
+        'bias': True, 
+        'root_weight': True
+    }
+
+    # model = aegnn.models.by_task(args.task)(args.model, args.dataset, num_classes=dm.num_classes,
+    #                                         img_shape=dm.dims, dim=args.dim, learning_rate=args.init_lr, weight_decay = args.weight_decay, bias=True, root_weight=True)
+    model = aegnn.models.by_task(args.task)(**model_args)
     
     # training the async model (?), copy from flop.py
     # edge_attr = torch_geometric.transforms.Cartesian(cat=False, max_value=10.0)
@@ -83,12 +121,18 @@ def main(args):
     ]
 
     trainer_kwargs = dict()
-    trainer_kwargs["gpus"] = [args.gpu] if args.gpu is not None else None
+    # trainer_kwargs["gpus"] = [args.gpu] if args.gpu is not None else None
     trainer_kwargs["profiler"] = "simple" if args.profile else False
     trainer_kwargs["weights_summary"] = "full"
     trainer_kwargs["track_grad_norm"] = 2 if args.log_gradients else -1
 
-    trainer = pl.Trainer.from_argparse_args(args, logger=logger, callbacks=callbacks, **trainer_kwargs)
+    if not args.cpu:
+        trainer_kwargs["accelerator"] = "gpu"
+        trainer_kwargs["devices"] = -1
+        trainer_kwargs["auto_select_gpus"] = True
+
+
+    trainer = pl.Trainer.from_argparse_args(args, max_epochs=train_args['max_epochs'], logger=logger, callbacks=callbacks, **trainer_kwargs)
     trainer.fit(model, datamodule=dm)
 
 
--- scripts/train_n_calc.py
+++ scripts/train_n_calc.py
@@ -32,10 +32,23 @@ batch_size = 64
 init_lr = 0.001
 w_decay = 0.005
 
+# sweep_config = {
+#     'method': 'random',
+#     'name': 'sweep',
+#     'metric': {'goal': 'maximize', 'name': "Val/Accuracy"},
+#     'parameters': 
+#     {
+#         'max_epochs': {'values': [30,50,100]},
+#         'learning_rate': {'max': 0.01, 'min': 0.0005},
+#         'batch_size': {'values': [16, 32, 64, 128]},
+#         'weight_decay': {'max': 0.01, 'min': 0.001}
+#      }
+# }
 
-
-cmd_train = f'python3 ../scripts/train.py {model} --task {task} --dataset {dataset} --gpu {which_gpu} --batch-size {batch_size} --dim {dim} --max-epochs {max_epochs} --init-lr {init_lr} --weight-decay {w_decay}'
+# eg: CUDA_VISIBLE_DEVICES=5 python3 ../scripts/train.py graph_res --task recognition --dataset ncars --gpu 5 --batch-size 64 --dim 3 --max-epochs 2 --init-lr 0.001 --weight-decay 0.005
+cmd_train = f'CUDA_VISIBLE_DEVICES={which_gpu} python3 ../scripts/train.py {model} --task {task} --dataset {dataset} --batch-size {batch_size} --dim {dim} --max-epochs {max_epochs} --init-lr {init_lr} --weight-decay {w_decay}'
 cmd_cpresult = f'python get_latest_results.py'
+# eg: python ../evaluation/accuracy_per_events.py /users/yyang22/thesis/aegnn_project/aegnn_results/training_results/latest/latest_model.pt --device cuda --dataset ncars
 cmd_accuracy = f'python ../evaluation/accuracy_per_events.py /users/yyang22/thesis/aegnn_project/aegnn_results/training_results/latest/latest_model.pt --device {device} --dataset {dataset}'
 cmd_csv = f'python pkl2csv.py'
 
