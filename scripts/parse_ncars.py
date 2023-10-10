d5b2ba10081ccebb0562cce298ed000e99efe7d5
whybear0510 <827239805@qq.com>
2023-10-10 17:38:10 +0200

    new parse_ncars

--- scripts/parse_ncars.py
+++ scripts/parse_ncars.py
@@ -59,126 +59,151 @@ def load_atis_data(filename, flipX=0, flipY=0):
     return td_data, header
 
 
-# output
-source_folder_train = '/space/yyang22/datasets/data/storage/original_ncars/Prophesee_Dataset_n_cars/n-cars_train'
-source_folder_test = '/space/yyang22/datasets/data/storage/original_ncars/Prophesee_Dataset_n_cars/n-cars_test'
-ttshuffled = '/space/yyang22/datasets/data/storage/ncars/ttshuffled'
+
+# def parse_ncars_to_txt(src_ds_path: str, dst_ds_path: str):
+#     sequence_counter = 0
+#     # traverse binary files in NCars
+#     for root, dirs, files in os.walk(src_ds_path):
+#         for dir_name in dirs:
+#             subfolder = os.path.join(root, dir_name)
+
+#             if 'background' in subfolder:
+#                 is_car = 0
+#             elif 'cars' in subfolder:
+#                 is_car = 1
+#             else:
+#                 continue
+
+#             for file_name in tqdm(os.listdir(subfolder), desc=dir_name):
+#                 if file_name.endswith('.dat'):
+#                     binary_file = os.path.join(subfolder, file_name)
+
+#                     td_data, _ = load_atis_data(binary_file)
+
+#                     # make events.txt
+#                     sequence_folder = os.path.join(dst_ds_path, f'sequence_{sequence_counter}')
+#                     os.makedirs(sequence_folder, exist_ok=True)
+
+#                     events_file = os.path.join(sequence_folder, 'events.txt')
+#                     with open(events_file, 'w') as txt_file:
+#                         for i in range(len(td_data['ts'])):
+#                             formatted_line = "{:.18e} {:.18e} {:.18e} {:.18e}".format(td_data['x'][i], td_data['y'][i], td_data['ts'][i], td_data['p'][i])
+#                             txt_file.write(formatted_line + '\n')
+
+#                     # make is_car.txt
+#                     is_car_file = os.path.join(sequence_folder, 'is_car.txt')
+#                     with open(is_car_file, 'w') as txt_file:
+#                         txt_file.write(str(is_car))
+
+#                     # add counter
+#                     sequence_counter += 1
+#     print(f'Parsed {sequence_counter} ncars samples to txt')
+
+def parse_ncars_to_txt(src_ds_path: str, dst_ds_path: str):
+    sequence_counter = 0
+    # Combine and shuffle the files in background and cars folders
+    combined_file_list = []
+
+    # Traverse binary files in NCars
+    for root, dirs, files in os.walk(src_ds_path):
+        for dir_name in dirs:
+            subfolder = os.path.join(root, dir_name)
+
+            if 'background' in subfolder:
+                is_car = 0
+            elif 'cars' in subfolder:
+                is_car = 1
+            else:
+                continue
+
+            # Collect the list of files in the current subfolder
+            file_list = [file_name for file_name in os.listdir(subfolder) if file_name.endswith('.dat')]
+
+            # Add files to the combined list
+            combined_file_list.extend([(file, is_car) for file in file_list])
+
+    # Shuffle the combined list
+    random.shuffle(combined_file_list)
+
+    # Process the shuffled files
+    for (file_name, is_car) in tqdm(combined_file_list, desc='Processing shuffled files'):
+        subfolder = 'background' if is_car == 0 else 'cars'
+
+        binary_file = os.path.join(src_ds_path, subfolder, file_name)
+
+        td_data, _ = load_atis_data(binary_file)
+
+        # make events.txt
+        sequence_folder = os.path.join(dst_ds_path, f'sequence_{sequence_counter}')
+        os.makedirs(sequence_folder, exist_ok=True)
+
+        events_file = os.path.join(sequence_folder, 'events.txt')
+        with open(events_file, 'w') as txt_file:
+            for i in range(len(td_data['ts'])):
+                formatted_line = "{:.18e} {:.18e} {:.18e} {:.18e}".format(td_data['x'][i], td_data['y'][i], td_data['ts'][i], td_data['p'][i])
+                txt_file.write(formatted_line + '\n')
+
+        # make is_car.txt
+        is_car_file = os.path.join(sequence_folder, 'is_car.txt')
+        with open(is_car_file, 'w') as txt_file:
+            txt_file.write(str(is_car))
+
+        # Increment the counter
+        sequence_counter += 1
+
+    print(f'Parsed {sequence_counter} ncars samples to txt')
+
+if __name__ == '__main__':
+
+    AEGNN_DATA_DIR = "/space/yyang22/datasets/data/storage/"
+
+    src_train = os.path.join(AEGNN_DATA_DIR, 'original_ncars/Prophesee_Dataset_n_cars/n-cars_train')
+    src_test  = os.path.join(AEGNN_DATA_DIR, 'original_ncars/Prophesee_Dataset_n_cars/n-cars_test')
+    dst_train = os.path.join(AEGNN_DATA_DIR, 'ncars/training')
+    dst_test  = os.path.join(AEGNN_DATA_DIR, 'ncars/test')
+
+    if not os.path.exists(dst_train):
+        os.makedirs(dst_train)
+    if not os.path.exists(dst_test):
+        os.makedirs(dst_test)
+
+    print('Parsing training set')
+    parse_ncars_to_txt(src_train, dst_train)
+
+    print('Parsing test set')
+    parse_ncars_to_txt(src_test, dst_test)
 
 
-sequence_counter = 0
-train_counter = 0
-# traverse binary files in NCars
-for root, dirs, files in os.walk(source_folder_train):
-    for dir_name in dirs:
-        subfolder = os.path.join(root, dir_name)
+    # # Manually split validation set
 
-        if 'background' in subfolder:
-            is_car = 0
-        elif 'cars' in subfolder:
-            is_car = 1
-        else:
-            continue
+    # # Set a random seed for reproducibility
+    # seed = 12345
+    # random.seed(seed)
+    # print(f'Manually split validation set. Seed {seed}')
 
+    # # Define the paths to the training and validation folders
+    # dst_val = '/space/yyang22/datasets/data/storage/ncars/validation'
 
-        for file_name in tqdm(os.listdir(subfolder), desc=dir_name):
-            if file_name.endswith('.dat'):
-                binary_file = os.path.join(subfolder, file_name)
+    # # Ensure the validation folder exists; create it if it doesn't
+    # if not os.path.exists(dst_val):
+    #     os.makedirs(dst_val)
 
-                td_data, _ = load_atis_data(binary_file)
+    # # Get a list of all files (samples) in the training folder
+    # training_files = os.listdir(dst_train)
 
-                # make events.txt
-                sequence_folder = os.path.join(ttshuffled, f'sequence_{sequence_counter}')
-                os.makedirs(sequence_folder, exist_ok=True)
+    # # Get a list of all subdirectories (samples) in the training folder
+    # training_samples = [sample for sample in os.listdir(dst_train) if os.path.isdir(os.path.join(dst_train, sample))]
 
-                events_file = os.path.join(sequence_folder, 'events.txt')
-                with open(events_file, 'w') as txt_file:
-                    for i in range(len(td_data['ts'])):
-                        formatted_line = "{:.18e} {:.18e} {:.18e} {:.18e}".format(td_data['x'][i], td_data['y'][i], td_data['ts'][i], td_data['p'][i])
-                        txt_file.write(formatted_line + '\n')
+    # # Calculate the number of samples to move (20%)
+    # num_samples_to_move = int(0.2 * len(training_samples))
 
-                # make is_car.txt
-                is_car_file = os.path.join(sequence_folder, 'is_car.txt')
-                with open(is_car_file, 'w') as txt_file:
-                    txt_file.write(str(is_car))
+    # # Randomly select the samples to move
+    # samples_to_move = random.sample(training_samples, num_samples_to_move)
 
-                # add counter
-                sequence_counter += 1
-                train_counter += 1
-print(f'Parsed {train_counter} ncars samples for training')
+    # # Move the selected samples (entire directories) to the validation folder
+    # for sample in samples_to_move:
+    #     source_path = os.path.join(dst_train, sample)
+    #     destination_path = os.path.join(dst_val, sample)
+    #     shutil.move(source_path, destination_path)
 
-test_counter = 0
-# traverse binary files in NCars
-for root, dirs, files in os.walk(source_folder_test):
-    for dir_name in dirs:
-        subfolder = os.path.join(root, dir_name)
-
-        if 'background' in subfolder:
-            is_car = 0
-        elif 'cars' in subfolder:
-            is_car = 1
-        else:
-            continue
-
-
-        for file_name in tqdm(os.listdir(subfolder), desc=dir_name):
-            if file_name.endswith('.dat'):
-                binary_file = os.path.join(subfolder, file_name)
-
-                td_data, _ = load_atis_data(binary_file)
-
-                # make events.txt
-                sequence_folder = os.path.join(ttshuffled, f'sequence_{sequence_counter}')
-                os.makedirs(sequence_folder, exist_ok=True)
-
-                events_file = os.path.join(sequence_folder, 'events.txt')
-                with open(events_file, 'w') as txt_file:
-                    for i in range(len(td_data['ts'])):
-                        formatted_line = "{:.18e} {:.18e} {:.18e} {:.18e}".format(td_data['x'][i], td_data['y'][i], td_data['ts'][i], td_data['p'][i])
-                        txt_file.write(formatted_line + '\n')
-
-                # make is_car.txt
-                is_car_file = os.path.join(sequence_folder, 'is_car.txt')
-                with open(is_car_file, 'w') as txt_file:
-                    txt_file.write(str(is_car))
-
-                # add counter
-                sequence_counter += 1
-                test_counter += 1
-print(f'Parsed {test_counter} ncars samples for test')
-print(f'Total {train_counter+test_counter} samples parsed; {len(os.listdir(ttshuffled))} files in {ttshuffled}')
-
-
-
-# Random re-assign
-training_count = 14417
-validation_count = 4806
-test_count = 4806
-print(f'Now random re-assigning {training_count} for training, {validation_count} for val, and {test_count} for test')
-seed = 12345
-random.seed(seed)
-print(f'Seed {seed}')
-
-
-source_folder = ttshuffled
-output_folder = '/space/yyang22/datasets/data/storage/ncars'
-
-all_files = os.listdir(source_folder)
-random.shuffle(all_files)
-
-
-subfolders = ["training", "validation", "test"]
-for subfolder in subfolders:
-    subfolder_path = os.path.join(output_folder, subfolder)
-    os.makedirs(subfolder_path, exist_ok=False)
-
-
-for i, subfolder in enumerate(subfolders):
-    count = training_count if i == 0 else validation_count if i == 1 else test_count
-    for j in tqdm(range(count), desc=subfolder):
-        if not all_files:
-            break
-        source_file = all_files.pop()
-        source_file_path = os.path.join(source_folder, source_file)
-        dest_file_path = os.path.join(output_folder, subfolder, source_file)
-        shutil.move(source_file_path, dest_file_path)
-
+    # print(f"Moved {len(samples_to_move)} samples (directories) to the validation folder.")
